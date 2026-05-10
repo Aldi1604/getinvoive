5e17dc7f6acc88be591fc0d98f098b907e24450d
Aldi1604 <aldiafriyan95@gmail.com>
2026-05-10 23:19:36 +0700

    restore index

--- server/index.js
+++ server/index.js
@@ -1,31 +1,34 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+
 import authRoutes from "./routes/authRoutes.js"
 import invoiceRoutes from "./routes/invoiceRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
 import paymentRoutes from "./routes/paymentRoutes.js"
 
 dotenv.config()
 
 const app = express()
 
+const PORT = process.env.PORT || 5000
+
 app.use(cors({
-  origin: "https://getinvoice.my.id",
-  credentials: true
+  origin: "*",
 }))
+
 app.use(express.json())
+
+// ROUTES
 app.use("/api/auth", authRoutes)
 app.use("/api/invoice", invoiceRoutes)
-app.use("/api/user", userRoutes)
 app.use("/api/payment", paymentRoutes)
 
+// ROOT
 app.get("/", (req, res) => {
-  res.send("API aktif")
+  res.send("API RUNNING 🚀")
 })
 
-const PORT = 5000
+// START SERVER
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
-  console.log(process.env.DATABASE_URL)
 })
